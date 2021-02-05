994e12d219061883c9b990a9cbc38d331ac27ade
Nikola-Gamatech <nikola.barac.gamatech@gmail.com>
2021-02-05 23:11:52 +0100

    error reporting

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define('ROOT', __DIR__);
 define('SYSTEM', ROOT . DIRECTORY_SEPARATOR . 'System');
 define('APPLICATION', ROOT . DIRECTORY_SEPARATOR . 'Application');
